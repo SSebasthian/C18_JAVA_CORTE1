82d7095be23d2a4fbc06f5de2f54ac6dccfc659b
SSEBASTHIAN <ssebasthian.perez@gmail.com>
2023-10-10 20:29:34 -0500

    Bloque 5

---
+++ src/main/java/B5/Ejercicio_Clase_B5.java
@@ -0,0 +1,14 @@
+package B5;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+
+public class Ejercicio_Clase_B5 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+}
